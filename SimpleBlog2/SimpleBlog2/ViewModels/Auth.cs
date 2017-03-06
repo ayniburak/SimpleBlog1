2e0ced594ebe4ac0cdf286c808fc33ee92bdfa09
buraksiriner <buraksiriner2015@gmail.com>
2017-03-06 18:27:59 +0200

    eighth commit

---
+++ SimpleBlog2/SimpleBlog2/ViewModels/Auth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog2.ViewModels
+{
+    public class AuthLogin
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+    public class AuthIndex
+    {
+
+    }
+}
