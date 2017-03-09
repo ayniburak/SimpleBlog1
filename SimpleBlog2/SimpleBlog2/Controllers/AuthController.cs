ddfcfff7d390f95f5fa483866f228b29e8700817
buraksiriner <buraksiriner2015@gmail.com>
2017-03-09 18:13:02 +0200

    Strongly Typed commit

--- SimpleBlog2/SimpleBlog2/Controllers/AuthController.cs
+++ SimpleBlog2/SimpleBlog2/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog2.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,15 @@ namespace SimpleBlog2.Controllers
 {
     public class AuthController : Controller
     {
-        public ActionResult Login()
+        public ActionResult Login()  //bu get isteği
         {
             return View();
         }
+
+        [HttpPost] //bunun varlıgı altındaki fonksiyonun post olduğu zaman çalışacağını belirler.
+        public ActionResult Login(AuthLogin form) //bu post isteği yukarıdaki http kısmından form getirmesini saglıyor
+        {
+            return Content("Hi "+form.Username+" - your password : " + form.Password);
+        }
     }
-}
+}   
